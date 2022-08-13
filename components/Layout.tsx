870a64fcf105e7f4fabcee77d154b3d23781ba32
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-08-13 19:13:11 +0800

    feat: improved darkmode

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,11 +1,10 @@
 import type { ReactNode } from "react";
-import styles from "../styles/Layout.module.css";
 import Header from "./Header";
 import React from "react";
 import Main from "./Main";
 
 const Layout = ({ children }: { children: ReactNode }) => (
-  <div className={styles.container}>
+  <div>
     <Header></Header>
     <Main>{children}</Main>
   </div>
