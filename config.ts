ba8e78ad313ead0f6168261e50738f0f93ea0742
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-08-16 13:42:41 +0800

    feat: improve tablet ui

--- config.ts
+++ config.ts
@@ -1,35 +1,49 @@
 const config = [
-    {
-        title: "Timochan",
-        link: "https://timochan.cn",
-        desc: "Let's start learning",
-        avatar:
-            "https://pic.rmb.bdstatic.com/bjh/4d22b9343b30540efed365a52262be87.png",
-    },
-    {
-        title: "Expliyh",
-        link: "https://www.mt6735.top/",
-        avatar:
-            "https://cdn.jsdelivr.net/gh/expliyh/static_expli_top@main/static/favicon/afavicon.ico",
-    },
-    {
-        title: "IbeXlwy",
-        link: "https://ibexlwy.com/",
-        avatar: "https://ibexlwy.com/photo/head.png",
-        desc: "人活着，不能为了贡品",
-    },
-    {
-        title: "JiHua",
-        link: "https://jihau.com/",
-        avatar: "https://pic.rmb.bdstatic.com/bjh/aa2e26a532fed409885b6506eeeebcee.png"
-    },
-] as data[]
+  {
+    title: "Timochan",
+    link: "https://timochan.cn",
+    desc: "Let's start learning",
+    avatar:
+      "https://pic.rmb.bdstatic.com/bjh/4d22b9343b30540efed365a52262be87.png",
+  },
+  {
+    title: "Expliyh",
+    link: "https://www.mt6735.top/",
+    avatar:
+      "https://cdn.jsdelivr.net/gh/expliyh/static_expli_top@main/static/favicon/afavicon.ico",
+  },
+  {
+    title: "IbeXlwy",
+    link: "https://ibexlwy.com/",
+    avatar: "https://ibexlwy.com/photo/head.png",
+    desc: "人活着，不能为了贡品",
+  },
+  {
+    title: "JiHua",
+    link: "https://jihau.com/",
+    avatar:
+      "https://pic.rmb.bdstatic.com/bjh/aa2e26a532fed409885b6506eeeebcee.png",
+  },
+  {
+    title: "江村暮",
+    link: "https://rene.wang",
+    avatar:
+      "https://pic.rmb.bdstatic.com/bjh/0d1dda383594903e2ca3b42a38167550.png",
+    desc: "写在kindle里的博客",
+  },
+  {
+    title: "Signaliks",
+    link: "https://iks.moe/",
+    avatar: "https://static.gmx0.cn/myIndex/avatar.jpg",
+    desc: "Operator",
+  },
+] as data[];
 
-export default config
+export default config;
 
 export type data = {
-    avatar: string;
-    title: string;
-    desc?: string;
-    link: string;
+  avatar: string;
+  title: string;
+  desc?: string;
+  link: string;
 };
