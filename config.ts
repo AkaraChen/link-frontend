d0e7f14d44aa6511835e087393f3db2842b48e49
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-10-28 12:40:04 +0800

    update

--- config.ts
+++ config.ts
@@ -40,8 +40,15 @@ const config = [
   {
     title: "喵二の小博客",
     link: "https://www.miaoer.xyz",
-    avatar: "https://dd-static.jd.com/ddimg/jfs/t1/25080/8/19573/58432/63295dbcE35bcebb8/4296442afe71ac89.png",
+    avatar:
+      "https://dd-static.jd.com/ddimg/jfs/t1/25080/8/19573/58432/63295dbcE35bcebb8/4296442afe71ac89.png",
     desc: "缘，妙不可言",
+  },
+  {
+    title: "秉松博客",
+    link: "https://blog.iucky.cn/",
+    avatar: "https://q1.qlogo.cn/g?b=qq&nk=1596355173&s=640",
+    desc: "有秉性且正直的松",
   },
 ] as data[];
 
