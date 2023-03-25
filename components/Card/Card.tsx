c3aec9e1054ae707d6c59dbf1abcc41dfc274844
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2023-03-25 18:38:31 +0800

    feat: use `next/link`

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -2,40 +2,44 @@ import { FC, ReactNode, useState } from "react";
 import styles from "./Card.module.css";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 const Card: FC<CardProps> = ({ children, image, desc, link }) => {
   const [hover, setHover] = useState(false);
   return (
-    <motion.a
-      href={link}
-      className={styles.container}
-      target="_blank"
-      rel="noreferrer"
+    <motion.div
       onHoverStart={() => setHover(true)}
       onHoverEnd={() => setHover(false)}
     >
-      <Image
-        className={styles.image}
-        src={image}
-        width={70}
-        height={70}
-        alt={children as string}
-      />
-      <section className={styles.section}>
-        <p className={styles.title}>{children}</p>
-        <span className={styles.desc}>{desc}</span>
-      </section>
-      {hover && (
-        <motion.div
-          className={styles.hover}
-          layoutId={"cardHover"}
-          initial={{
-            opacity: 0.05,
-            backgroundColor: "black",
-          }}
+      <Link
+        className={styles.container}
+        href={link}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <Image
+          className={styles.image}
+          src={image}
+          width={70}
+          height={70}
+          alt={children as string}
         />
-      )}
-    </motion.a>
+        <section className={styles.section}>
+          <p className={styles.title}>{children}</p>
+          <span className={styles.desc}>{desc}</span>
+        </section>
+        {hover && (
+          <motion.div
+            className={styles.hover}
+            layoutId={"cardHover"}
+            initial={{
+              opacity: 0.05,
+              backgroundColor: "black",
+            }}
+          />
+        )}
+      </Link>
+    </motion.div>
   );
 };
 
